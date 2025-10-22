6d6e28992409510ad3981f8914cba3921c4a57f6
Darien Miller <darienmiller88@yahoo.com>
2025-10-22 14:51:50 -0400

    feat: added student profile route

--- AttendanceTrackerApp/obj/Debug/net9.0/AttendanceTrackerApp.AssemblyInfo.cs
+++ AttendanceTrackerApp/obj/Debug/net9.0/AttendanceTrackerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AttendanceTrackerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9301892442377374c7b1c56d54e7abfae20058d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8529886a3fbe05e6ffc832c225ba3009821797")]
 [assembly: System.Reflection.AssemblyProductAttribute("AttendanceTrackerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AttendanceTrackerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
