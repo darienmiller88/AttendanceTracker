f997414d5de0889b8c2a78e763b3421a3cc65285
Darien Miller <darienmiller88@yahoo.com>
2025-10-23 23:41:39 -0400

    styles: added rounded profile pics to student profiles

--- AttendanceTrackerApp/obj/Debug/net9.0/AttendanceTrackerApp.AssemblyInfo.cs
+++ AttendanceTrackerApp/obj/Debug/net9.0/AttendanceTrackerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AttendanceTrackerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d6e28992409510ad3981f8914cba3921c4a57f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+832b396fd0d56fe190afabab77fce27a515d9236")]
 [assembly: System.Reflection.AssemblyProductAttribute("AttendanceTrackerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AttendanceTrackerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
